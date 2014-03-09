ff7c9e8f30fca0ce55b1794c8110afe07904abe3
Jobert Abma <j.abma@online24.nl>
2014-03-09 11:41:27 +0100

    Initial test version of Conduit class in Ruby

---
+++ lib/ruby-conduit.rb
@@ -0,0 +1,119 @@
+require 'uri'
+require 'json'
+require 'openssl'
+require 'net/http'
+require 'digest/sha1'
+
+class ConduitError < Exception
+end
+
+class Conduit
+  VERSION = '0.1'
+
+  def self.connect!
+    certificate = credentials['cert']
+    token       = Time.now.to_i
+
+    data = {
+      'authToken'     => token,
+      'authSignature' => hash(token, certificate),
+      'user'          => credentials['user'],
+    }
+
+    session = call 'conduit.connect', data
+
+    @connectionID = session['connectionID']
+    @sessionKey   = session['sessionKey']
+
+    true
+  end
+
+  def self.session
+    {
+      'sessionKey'   => @sessionKey,
+      'connectionID' => @connectionID,
+    }
+  end
+
+  def self.connected?
+    @sessionKey && @connectionID
+  end
+
+  def self.call(method, data = {})
+    request = Net::HTTP::Post.new "#{conduit_uri.path}#{method}",
+      static_http_headers
+
+    request.set_form_data request_body(data)
+
+    response = JSON.parse connection.request(request).body
+
+    unless response['error_code'].nil?
+      fail ConduitError, "#{response['error_info']} (#{response['error_code']})"
+    end
+
+    response['result']
+  end
+
+
+  def self.connection
+    new_connection = Net::HTTP.new conduit_uri.host, conduit_uri.port
+
+    new_connection.use_ssl = true
+    new_connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    new_connection
+  end
+
+  private
+
+  def self.static_http_headers
+    headers = {
+      'User-Agent'   => user_agent,
+      'Content-Type' => 'application/x-www-form-urlencoded'
+    }
+  end
+
+  def self.conduit_uri
+    URI.parse host
+  end
+
+  def self.user_agent
+    "ruby-conduit/#{VERSION}"
+  end
+
+  def self.conduit_host
+    config['hosts'].first
+  end
+
+  def self.host
+    conduit_host[0]
+  end
+
+  def self.credentials
+    conduit_host[1]
+  end
+
+  def self.config
+    JSON.parse(File.read('/Users/jobertabma/.arcrc'))
+  end
+
+  def self.hash(token, certificate)
+    Digest::SHA1.hexdigest "#{token}#{certificate}"
+  end
+
+  def self.request_body(data = {})
+    data.merge! __conduit__: session if connected?
+
+    {
+      'params'      => data.to_json,
+      'output'      => 'json',
+      'host'        => host,
+      '__conduit__' => true
+    }
+  end
+end
+
+Conduit.connect!
+
+puts Conduit.call 'differential.getrevision',
+  revision_id: '1337'
